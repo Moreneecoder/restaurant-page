d9c9c9ae3af5eaa0955f873e8101badac3cc078a
Morenikeji Bello <caeserselassie@gmail.com>
2021-07-14 16:27:04 +0100

    Install and setup css loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import _ from 'lodash';
+import './style.css';
+
 function component() {
     const element = document.createElement('div');
     const btn = document.createElement('button');
@@ -6,27 +9,6 @@ function component() {
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
-    // const myIcon = new Image();
-    // myIcon.src = Icon;
-
-    // element.appendChild(myIcon);
-    
-    // const paragraph = document.createElement('p');
-    // paragraph.innerHTML = 'Check am find!'
-    // element.appendChild(paragraph)
-
-    // const paragraph2 = document.createElement('p');
-    // paragraph2.innerHTML = 'Who dey zuzuzu!'
-    // element.appendChild(paragraph2)
-
-    // console.log(Data);
-    // console.log(Notes);
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
-  
     return element;
   }
   
