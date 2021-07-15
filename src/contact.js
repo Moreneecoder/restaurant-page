b8469d75745cc3e881643a89a308ac2b2f3fedba
Morenikeji Bello <caeserselassie@gmail.com>
2021-07-16 00:31:09 +0100

    Fix linter errors

--- src/contact.js
+++ src/contact.js
@@ -1,26 +1,25 @@
 export default function contactComponent() {
   const contactWrapper = document.createElement('div');
   contactWrapper.id = 'contact-wrapper';
-  contactWrapper.classList.add('pt-5')
+  contactWrapper.classList.add('pt-5');
 
-  const contactCard = document.createElement('div')
-  contactCard.classList.add('card', 'col-md-6', 'mx-auto', 'text-center', 'py-3')
+  const contactCard = document.createElement('div');
+  contactCard.classList.add('card', 'col-md-6', 'mx-auto', 'text-center', 'py-3');
 
-  const headline = document.createElement('h3')
-  headline.classList.add('fw-bolder')
-  headline.textContent = 'Contact Us'
+  const headline = document.createElement('h3');
+  headline.classList.add('fw-bolder');
+  headline.textContent = 'Contact Us';
 
-  const telephone = document.createElement('h5')
-  telephone.textContent = 'Tel: 08123456789'
+  const telephone = document.createElement('h5');
+  telephone.textContent = 'Tel: 08123456789';
 
-  const email = document.createElement('h5')
-  email.textContent = 'Email: chopngo@example.com'
+  const email = document.createElement('h5');
+  email.textContent = 'Email: chopngo@example.com';
 
+  contactCard.appendChild(headline);
+  contactCard.appendChild(telephone);
+  contactCard.appendChild(email);
+  contactWrapper.appendChild(contactCard);
 
-  contactCard.appendChild(headline)
-  contactCard.appendChild(telephone)
-  contactCard.appendChild(email)
-  contactWrapper.appendChild(contactCard)
-  
   return contactWrapper;
 }
