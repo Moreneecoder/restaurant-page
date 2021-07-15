2c2aa5e3921a03dec7070486fa731db8b1848516
Morenikeji Bello <caeserselassie@gmail.com>
2021-07-16 00:30:24 +0100

    Add display structure to contact tab

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,26 @@
 export default function contactComponent() {
   const contactWrapper = document.createElement('div');
   contactWrapper.id = 'contact-wrapper';
+  contactWrapper.classList.add('pt-5')
 
-  const paragraph = document.createElement('p');
-  paragraph.textContent = 'Contact Wrapper';
-  contactWrapper.appendChild(paragraph);
+  const contactCard = document.createElement('div')
+  contactCard.classList.add('card', 'col-md-6', 'mx-auto', 'text-center', 'py-3')
 
+  const headline = document.createElement('h3')
+  headline.classList.add('fw-bolder')
+  headline.textContent = 'Contact Us'
+
+  const telephone = document.createElement('h5')
+  telephone.textContent = 'Tel: 08123456789'
+
+  const email = document.createElement('h5')
+  email.textContent = 'Email: chopngo@example.com'
+
+
+  contactCard.appendChild(headline)
+  contactCard.appendChild(telephone)
+  contactCard.appendChild(email)
+  contactWrapper.appendChild(contactCard)
+  
   return contactWrapper;
 }
