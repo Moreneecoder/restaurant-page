dc704ee72ad98ee09a8a9d6e8edf63425aaaeb38
Morenikeji Bello <caeserselassie@gmail.com>
2021-07-15 22:30:11 +0100

    Fix linter errors

--- src/menu.js
+++ src/menu.js
@@ -1,36 +1,36 @@
 const loadMealsTo = (menuRow) => {
-    const meals = [
-        {
-            type: 'African Dish',
-            name: 'Amala with Efo Riro and Assorted Meat',
-            description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Lorem Ipsum',
-            price: '2,500'
-        },
-        {
-            type: 'Beverage',
-            name: 'Inui Protein Shake',
-            description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Lorem Ipsum',
-            price: '1,400'
-        },
-        {
-            type: 'African Dish',
-            name: 'Pounded Yam with Egusi and Assorted Meat',
-            description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Lorem Ipsum',
-            price: '3,000'
-        },
-        {
-            type: 'Beverage',
-            name: 'Oreo Latte',
-            description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Lorem Ipsum',
-            price: '2,000'
-        },
-    ];
+  const meals = [
+    {
+      type: 'African Dish',
+      name: 'Amala with Efo Riro and Assorted Meat',
+      description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Lorem Ipsum',
+      price: '2,500',
+    },
+    {
+      type: 'Beverage',
+      name: 'Inui Protein Shake',
+      description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Lorem Ipsum',
+      price: '1,400',
+    },
+    {
+      type: 'African Dish',
+      name: 'Pounded Yam with Egusi and Assorted Meat',
+      description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Lorem Ipsum',
+      price: '3,000',
+    },
+    {
+      type: 'Beverage',
+      name: 'Oreo Latte',
+      description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Lorem Ipsum',
+      price: '2,000',
+    },
+  ];
 
-    meals.forEach((meal) => {
-        const mealCol = document.createElement('div');
-        mealCol.classList.add('meal', 'col-12', 'col-md-6', 'mb-5');
+  meals.forEach((meal) => {
+    const mealCol = document.createElement('div');
+    mealCol.classList.add('meal', 'col-12', 'col-md-6', 'mb-5');
 
-        mealCol.innerHTML = `
+    mealCol.innerHTML = `
             <span class="theme-bg text-white p-1 rounded">${meal.type}</span>
             <div class="mt-1 meal-col-flex">
                 <div class="me-2">
@@ -42,11 +42,11 @@ const loadMealsTo = (menuRow) => {
             </div>
         `;
 
-        menuRow.appendChild(mealCol);
-    })
+    menuRow.appendChild(mealCol);
+  });
 
-    return menuRow
-}
+  return menuRow;
+};
 
 export default function menuComponent() {
   const menuWrapper = document.createElement('div');
